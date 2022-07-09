dc7ddf86380a3fb7ee112204f94acf8deb1c80e9
dacem <74594956+dacem@users.noreply.github.com>
2022-07-09 11:31:46 +0300

    Pabeigta 7.nodarbiba

--- 7_diena/requests/ss_vaksana.py
+++ 7_diena/requests/ss_vaksana.py
@@ -3,51 +3,40 @@ import time
 from bs4 import BeautifulSoup as bs
 import csv
 
-ADRESE = "https://www.ss.lv/lv/transport/cars/today/"
-
-
 def saglabat_lapu(adrese, fails):
     pieprasijums = requests.get(adrese)
 
     if pieprasijums.status_code == 200:
         lapa = pieprasijums.text
 
-        with open(fails, "w", encoding="utf-8") as f:
-            f.write(lapa)
+        with open(fails, "w", encoding="utf-8") as fails:
+            fails.write(lapa)
+
     else:
         print("Radās kļūda lapas pieprasīšanā! Kods:", pieprasijums.status_code)
-        return
 
-#saglabat_lapu(ADRESE, "7_diena/requests/lapas/lapa_1.html")
-
-#ADRESE = "https://www.ss.com/lv/transport/cars/today/page1.html"
-#ADRESE = "https://www.ss.com/lv/transport/cars/today/page2.html"
-#ADRESE = "https://www.ss.com/lv/transport/cars/today/page3.html"
-#ADRESE = "https://www.ss.com/lv/transport/cars/today/page4.html"
+ADRESE = "https://www.ss.lv/lv/transport/cars/today/"
 
 def atvilkt_lapas(daudzums):
     for i in range(1, daudzums + 1):
         adrese = f"{ADRESE}page{i}.html"
         fails = f"7_diena/requests/lapas/lapa_{i}.html"
-       
+
         print("Pieprasījums uz", adrese, "--->", fails)
         saglabat_lapu(adrese, fails)
 
         time.sleep(2)
 
-#atvilkt_lapas(46)
-#kad atvilkts, aizkomentē, lai palaižot lapu, nesāk vilkt no jauna
+# atvilkt_lapas(46)
 
 def info(htmlDatne):
-    with open(htmlDatne, "r", encoding='utf-8') as f:
+    with open(htmlDatne, "r", encoding="utf-8") as f:
         html = f.read()
 
     zupa = bs(html, "html.parser")
 
     tabulas = zupa.find_all("table")
     
-    
-    #print(tabulas[4])
     autoTabula = tabulas[4]
 
     autoRindas = autoTabula.find_all("tr")
@@ -57,19 +46,17 @@ def info(htmlDatne):
     for rinda in autoRindas[1:-1]:
         auto = {}
         # print(rinda)
-        # print("============================")
-        # print("============================")
-        # print("============================")
-        # print("============================")
+        # print("========================")
+        # print("========================")
+        # print("========================")
 
         lauki = rinda.find_all("td")
 
-        if lauki[4].text == "-" or lauki:
+        if lauki[4].text == "-" or lauki[6].text == "-" or not lauki[3].br:
             continue
-
+        
         auto['gads'] = lauki[4].text
 
-        print(lauki[5].text)
         tilpums = lauki[5].text
 
         if "D" in tilpums:
@@ -79,57 +66,46 @@ def info(htmlDatne):
             auto["dzinejs"] = "hibrīds"
             auto["tilpums"] = tilpums[:-1]
         elif "E" in tilpums:
-            #ignorējam elektromobīļus
+            # ignorejam elektromobilus
             continue
         else:
-            auto["dzinejs"] = "benzins"
-            auto["tilpums"] = tilpums 
+            auto["dzinejs"] = "benzīns"
+            auto["tilpums"] = tilpums
 
-        
-        auto["nobraukums"] = lauki[6].text.replace("tūkst.", "")
+        auto["nobraukums"] = lauki[6].text.replace(" tūkst.", "")
 
-        # auto["cena"] = lauki[7].text.replace(",", "").replace("  eiro", "")
+        auto["cena"] = lauki[7].text.replace(",","").replace("  €","").replace("maiņai", "")
 
         lauki[3].br.replace_with("!")
         auto["marka"] = lauki[3].text.replace("!", " ")
         auto["razotajs"] = lauki[3].text.split("!")[0]
         auto["modelis"] = lauki[3].text.split("!")[1]
 
-        # print(auto)
-        
-        # exit()
-    
+        #print(auto)
+        #exit()
+
         dati.append(auto)
 
     return dati
 
-
-    # for tabula in tabulas:
-    #     print(tabula)
-    #     print("============================")
-    #     print("============================")
-    #     print("============================")
-    #     print("============================")
-
 def saglabat_datus(autoDati):
-    with open("7_diena/requests/ss_auto.csv", "w", encoding = "uft-8", newline="") as f:
-        kolonnas = ['razotajs', 'modelis']
-        w = csv.DictWriter(f, fieldnames=kolonnas)
-        w.writeheader
+    with open("7_diena/requests/ss_auto.csv", "w", encoding="utf-8", newline="") as f:
+        kolonas = ['razotajs', 'modelis', 'marka', 'gads', 'dzinejs', 'tilpums', 'nobraukums', 'cena']
+        w = csv.DictWriter(f, fieldnames=kolonas)
+        w.writeheader()
+
+        for auto in autoDati:
+            w.writerow(auto)
 
 
 def izvilkt_datus(daudzums):
     visiDati = []
-    for i in range(1, daudzums +1):
+    for i in range(1, daudzums + 1):
         fails = f"7_diena/requests/lapas/lapa_{i}.html"
         datnesDati = info(fails)
         visiDati = visiDati + datnesDati
 
-        print(visiDati)
-        saglabat_datus(visiDati)   
+    saglabat_datus(visiDati)
+
 
 izvilkt_datus(46)
-
-
-dati = info("7_diena/requests/lapas/lapa_1.html")
-print(dati)
